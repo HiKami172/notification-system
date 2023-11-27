e968bc79f76e43a83f29db7485581343f968f26f
DESKTOP-OFMMG60\HiKami <hikami172@gmail.com>
2023-11-28 00:08:52 +0100

    Add sample usage script and keys config file.

---
+++ keys.py
@@ -0,0 +1,8 @@
+# Email Creds
+sender_email = 'your_email'
+email_password = 'app_password'
+
+# Twilio Creds
+account_sid = 'account_sid'
+auth_token = 'auth_token'
+phone_number = 'twilio_phone_number'  # with plus sign
--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from notification import NotificationBuilder
 
 
 def main():
-    message = 'test message'
-    email = 'recipient'
+    heading = "Heading"
+    body = 'message'
+    email = 'target_email'
+    phone = 'target_phone'  # including plus sign
     builder = NotificationBuilder() \
-        .set_message(message) \
-        .set_delivery_method('email', email)
-    notification = builder.build()
-    notification.handle_request()
+        .set_heading(heading) \
+        .set_body(body) \
+        .add_delivery_method('email', email) \
+        .add_delivery_method('sms', phone)
+    builder.build().handle()
 
 
 if __name__ == '__main__':
